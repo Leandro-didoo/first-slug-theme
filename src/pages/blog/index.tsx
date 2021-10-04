bcbb6f6b80c108ea054f9820eb4b76d12bb1b738
mateus_brandao <mateus.brandao@wexark.com.br>
2021-10-04 09:06:05 -0300

    Problema com blog solucionado

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -9,6 +9,7 @@ import { CardBlog } from "../../components/CardBlog";
 import { Footer } from '../../components/Footer'
 import { useRouter } from 'next/router'
 import Link from 'next/link';
+import Head from 'next/head';
 
 if (process.browser) {
     require('materialize-css');
@@ -16,100 +17,113 @@ if (process.browser) {
 
 
 type BlogProps = {
+    page_data: PageData;
     blog: BlogType,
     footer: FooterType,
     navBar: NavType,
 }
 
-export default function Blog({ blog, footer, navBar }: BlogProps) {
+export default function Blog({ page_data, blog, footer, navBar }: BlogProps) {
     const router = useRouter()
     return (
-        <div className={style.blogPage}>
-            <Nav
-                color='#333'
-                colorTxt=""
-                data={navBar}
-            >
-                <li>
-                    <Link href="/">
-                        <a>Inicio</a>
+        <>
+            <Head>
+                <title>{page_data.title}</title>
+                <meta name="description" content={page_data.metadescription} />
+                <link rel="icon" href={page_data.icon ?? "/favicon.ico"} />
 
-                    </Link>
-                </li>
-            </Nav>
-            <div className="container">
+                <title>Blog | {page_data.title}</title>
+                <link rel="icon" href={page_data.icon} />
+            </Head>
+            <div className={style.blogPage}>
+                <Nav
+                    color='#333'
+                    colorTxt=""
+                    data={navBar}
+                >
+                    <li>
+                        <Link href="/">
+                            <a>Inicio</a>
 
-                <header>
-                    <br />
-                    <br />
-                    <div className="row">
-                        <div className="col">
-                            <div className={style.titleBlog}>
-                                <h1>{blog.title}</h1>
-                            </div>
-                        </div>
-                        <div className="col s12">
-                            <div className={style.subTitle}>
-                                <button onClick={() => router.back()}><ChevronCircleLeft width={27} color="#333" /></button>
-                                <h2>{blog.subTitle}</h2>
+                        </Link>
+                    </li>
+                </Nav>
+                <div className="container" style={{ minHeight: '90vh'}}>
 
+                    <header>
+                        <br />
+                        <br />
+                        <div className="row">
+                            <div className="col">
+                                <div className={style.titleBlog}>
+                                    <h1>{blog.title}</h1>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col s12 m6 ">
-                            <div className={style.ImgEmphasis}>
-                                <img src={blog.data[0].wallpaper} alt="" />
-                            </div>
-                        </div>
-                        <div className="col s12 m6 ">
-                            <div className={style.InfoEmphasis}>
-                                <div className={style.item}>
-                                    <h3>{blog.data[0].title}</h3>
-                                    <small >
-                                        <Clock width={20} color="blue" />
-                                        {blog.data[0].date_formatted}
-                                    </small>
-                                    <div className={style.contnt} dangerouslySetInnerHTML={{ __html: `${blog.data[0].content}` }} />
+                            <div className="col s12">
+                                <div className={style.subTitle}>
+                                    <button onClick={() => router.back()}><ChevronCircleLeft width={27} color="#333" /></button>
+                                    <h2>{blog.subTitle}</h2>
 
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </header>
-                <main>
-                    <div className="row">
-
-                        {blog.data.map((content, index) => {
-                            if (index === 0) return;
-                            return (
-                                <div key={content.id} className={`col s12 m3`}>
-                                    <div className={style.cardBlog}>
-                                        <div className={style.img}>
-                                            <img src={content.image} alt={content.title} />
-                                        </div>
-                                        <div className={style.info}>
-                                            <small>
-                                                <Clock width={10} color="red" />
-                                                {content.date_formatted}
-                                            </small>
-                                            <Link href={`/blog/${content.slug}`}>
-                                                <a className={style.title}>{content.title}</a>
-                                            </Link>
-                                            <div className={style.contnt} dangerouslySetInnerHTML={{ __html: `${content.content}` }} />
-                                        </div>
+                        {blog.data.length > 0 ? (
+                            <div className="row">
+                                <div className="col s12 m6 ">
+                                    <div className={style.ImgEmphasis}>
+                                        <img src={blog.data[0].wallpaper} alt="" />
                                     </div>
                                 </div>
-                            )
-                        })}
+                                <div className="col s12 m6 ">
+                                    <div className={style.InfoEmphasis}>
+                                        <div className={style.item}>
+                                            <h3>{blog.data[0].title}</h3>
+                                            <small >
+                                                <Clock width={20} color="blue" />
+                                                {blog.data[0].date_formatted}
+                                            </small>
+                                            <div className={style.contnt} dangerouslySetInnerHTML={{ __html: `${blog.data[0].content}` }} />
 
-                    </div>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        ) : ''}
+                    </header>
+                    <main>
+                        <div className="row">
 
-                </main>
+                            {blog.data.map((content, index) => {
+                                if (index === 0) return;
+                                return (
+                                    <div key={content.id} className={`col s12 m3`}>
+                                        <div className={style.cardBlog}>
+                                            <div className={style.img}>
+                                                <img src={content.image} alt={content.title} />
+                                            </div>
+                                            <div className={style.info}>
+                                                <small>
+                                                    <Clock width={10} color="red" />
+                                                    {content.date_formatted}
+                                                </small>
+                                                <Link href={`/blog/${content.slug}`}>
+                                                    <a className={style.title}>{content.title}</a>
+                                                </Link>
+                                                <div className={style.contnt} dangerouslySetInnerHTML={{ __html: `${content.content}` }} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+
+                        </div>
+
+                    </main>
+                </div>
+                <Footer content={footer} />
+
             </div>
-            <Footer content={footer} />
-
-        </div>
+        </>
     )
 }
 
@@ -148,13 +162,14 @@ export const getStaticProps: GetStaticProps = async (ctx) => {
 
     const blog = {
         ...elements.cms_blog.data,
-        active: elements.cms_blog.active,
+        active: dataBlog.length === 0 ? false : elements.cms_blog.active,
         data: dataBlog
     };
     const footer = elements.footer.data;
 
     return {
         props: {
+            page_data,
             blog,
             navBar,
             footer
